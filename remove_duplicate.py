37df0273cea24ee1b5f36de36d703df00c35d3f6
G7O8 <5892ginafv@gmail.com>
2020-11-11 17:16:32 +0900

    Update file

--- remove_duplicate.py
+++ remove_duplicate.py
@@ -18,6 +18,21 @@ def search_and_remove_duplicate(df):
         df.iloc[gene_index, 0] = newname
     return df
 
+def remove_duplicate_if_same_lication(df):
+    # 遺伝子名(0行目)に重複があるものだけのlistをつくる
+    subdf = df[df[0].duplicated()]
+    # 重複がある遺伝子の一番上の遺伝子名を抽出
+    gene = subdf.iloc[0,0]
+    # 指定した遺伝子のindexを取得
+    dupdf = df[df[0]== gene]
+    gene_indexes = list(dupdf.index)
+    # <gene_name>_1, <gene_neme>_2 ...の形にして重複をなくす
+    for i, gene_index in enumerate(gene_indexes):
+        l = i + 1
+        newname = str(gene) + "_" + str(l)
+        df.iloc[gene_index, 0] = newname
+    return df
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="remove duplicate")
     parser.add_argument("filename" ,help="file path", type=str)
@@ -36,4 +51,3 @@ while any(df[0].duplicated()):
 
 else:
     df.to_csv(newfile, header=False, index=False)
-
